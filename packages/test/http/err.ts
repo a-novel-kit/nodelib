60a30f94b5c1c9659acf797b2a1936970499b859
Kushuh <geoffroy.vincent@agorastoryverse.com>
2026-01-22 01:21:52 +0100

    fix(test): improve error printing

--- packages/test/http/err.ts
+++ packages/test/http/err.ts
@@ -1,9 +1,17 @@
 import { isHttpStatusError } from "@a-novel-kit/nodelib-browser/http";
 
+function prettyRes(res: any): string {
+  try {
+    return JSON.stringify(res, null, 2);
+  } catch {
+    return `${res?.toString?.()}`;
+  }
+}
+
 export async function expectStatus(callback: Promise<any>, status: number) {
   const res = await callback.catch((err) => err);
 
   if (!isHttpStatusError(res, status)) {
-    throw new Error(`expected error with status ${status}, got: ${res.message ?? res}`);
+    throw new Error(`expected error with status ${status}, got: ${prettyRes(res)}`);
   }
 }
